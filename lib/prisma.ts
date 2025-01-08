01097255431d76591631eb2be2d99210f60c6ea3
pauseplayrepeat <mrsqueex@gmail.com>
2025-01-07 21:07:24 -0600

    update prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,12 @@
 import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: ['error'],
-    datasourceUrl: process.env.DATABASE_URL,
-  })
-}
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
+  prisma: PrismaClient | undefined
 }
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export { prisma } 
+export default prisma
+
